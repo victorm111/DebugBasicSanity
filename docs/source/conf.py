a4f5d39b2918b18412546bef48d1010defcf6fbb
victorm111 <103055123+victorm111@users.noreply.github.com>
2024-05-01 15:58:28 +0100

    updated requirements.txt  removed docs/built folder from git  added html_static_path = ["_static"] to sphinx conf.py, was causing tox docs error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,12 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.duration',
+extensions = [
+    'sphinx.ext.duration',
     'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',]
+    'sphinx.ext.autodoc']
+
+html_static_path = ["_static"]
 
 templates_path = ['_templates']
 exclude_patterns = []
